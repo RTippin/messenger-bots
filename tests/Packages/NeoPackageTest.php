0f5551ad0363f784a18aed6e51d36cded7f38f8a
Tippin <richard.tippin@gmail.com>
2021-12-10 00:28:21 -0500

    NeoPackageTest.php passing.

---
+++ tests/Packages/NeoPackageTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace RTippin\MessengerBots\Tests\Packages;
+
+use RTippin\Messenger\Facades\MessengerBots;
+use RTippin\MessengerBots\Bots\CommandsBot;
+use RTippin\MessengerBots\Bots\GiphyBot;
+use RTippin\MessengerBots\Bots\InviteBot;
+use RTippin\MessengerBots\Bots\KanyeBot;
+use RTippin\MessengerBots\Bots\LocationBot;
+use RTippin\MessengerBots\Bots\QuotableBot;
+use RTippin\MessengerBots\Bots\RandomImageBot;
+use RTippin\MessengerBots\Bots\WeatherBot;
+use RTippin\MessengerBots\Bots\WikiBot;
+use RTippin\MessengerBots\Bots\YoutubeBot;
+use RTippin\MessengerBots\Packages\NeoPackage;
+use RTippin\MessengerBots\Tests\MessengerBotsTestCase;
+
+class NeoPackageTest extends MessengerBotsTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        MessengerBots::registerPackagedBots([NeoPackage::class]);
+    }
+
+    /** @test */
+    public function it_gets_formatted_settings()
+    {
+        $expected = [
+            'alias' => 'neo_package',
+            'name' => 'Neo',
+            'description' => 'Bundles internet searching and general help topic actions.',
+            'avatar' => [
+                'sm' => '/messenger/assets/bot-package/sm/neo_package/avatar.jpg',
+                'md' => '/messenger/assets/bot-package/md/neo_package/avatar.jpg',
+                'lg' => '/messenger/assets/bot-package/lg/neo_package/avatar.jpg',
+            ],
+            'installs' => [
+                MessengerBots::getHandlers(GiphyBot::class)->toArray(),
+                MessengerBots::getHandlers(InviteBot::class)->toArray(),
+                MessengerBots::getHandlers(KanyeBot::class)->toArray(),
+                MessengerBots::getHandlers(CommandsBot::class)->toArray(),
+                MessengerBots::getHandlers(LocationBot::class)->toArray(),
+                MessengerBots::getHandlers(QuotableBot::class)->toArray(),
+                MessengerBots::getHandlers(RandomImageBot::class)->toArray(),
+                MessengerBots::getHandlers(WeatherBot::class)->toArray(),
+                MessengerBots::getHandlers(WikiBot::class)->toArray(),
+                MessengerBots::getHandlers(YoutubeBot::class)->toArray(),
+            ],
+        ];
+
+        $this->assertSame($expected, MessengerBots::getPackagedBots(NeoPackage::class)->toArray());
+    }
+
+    /** @test */
+    public function it_can_be_installed()
+    {
+        $thread = $this->createGroupThread($this->tippin);
+        $this->actingAs($this->tippin);
+
+        $this->postJson(route('api.messenger.threads.bots.packages.store', [
+            'thread' => $thread->id,
+        ]), [
+            'alias' => 'neo_package',
+        ])
+            ->assertSuccessful()
+            ->assertJson([
+                'actions_count' => 10,
+            ]);
+    }
+}
