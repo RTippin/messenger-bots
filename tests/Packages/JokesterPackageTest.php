4b0b43c2d8d7c52c67aa57db46195ab370945393
Tippin <richard.tippin@gmail.com>
2021-12-10 00:24:16 -0500

    JokesterPackageTest.php passing.

---
+++ tests/Packages/JokesterPackageTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace RTippin\MessengerBots\Tests\Packages;
+
+use RTippin\Messenger\Facades\MessengerBots;
+use RTippin\MessengerBots\Bots\ChuckNorrisBot;
+use RTippin\MessengerBots\Bots\DadJokeBot;
+use RTippin\MessengerBots\Bots\InsultBot;
+use RTippin\MessengerBots\Bots\JokeBot;
+use RTippin\MessengerBots\Bots\KnockBot;
+use RTippin\MessengerBots\Bots\ReactionBot;
+use RTippin\MessengerBots\Bots\YoMommaBot;
+use RTippin\MessengerBots\Packages\JokesterPackage;
+use RTippin\MessengerBots\Tests\MessengerBotsTestCase;
+
+class JokesterPackageTest extends MessengerBotsTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        MessengerBots::registerPackagedBots([JokesterPackage::class]);
+    }
+
+    /** @test */
+    public function it_gets_formatted_settings()
+    {
+        $expected = [
+            'alias' => 'jokester_package',
+            'name' => 'Jokester',
+            'description' => 'A bot with much to say! Bundles many joke telling abilities.',
+            'avatar' => [
+                'sm' => '/messenger/assets/bot-package/sm/jokester_package/avatar.jpg',
+                'md' => '/messenger/assets/bot-package/md/jokester_package/avatar.jpg',
+                'lg' => '/messenger/assets/bot-package/lg/jokester_package/avatar.jpg',
+            ],
+            'installs' => [
+                MessengerBots::getHandlers(ChuckNorrisBot::class)->toArray(),
+                MessengerBots::getHandlers(DadJokeBot::class)->toArray(),
+                MessengerBots::getHandlers(InsultBot::class)->toArray(),
+                MessengerBots::getHandlers(JokeBot::class)->toArray(),
+                MessengerBots::getHandlers(KnockBot::class)->toArray(),
+                MessengerBots::getHandlers(ReactionBot::class)->toArray(),
+                MessengerBots::getHandlers(YoMommaBot::class)->toArray(),
+            ],
+        ];
+
+        $this->assertSame($expected, MessengerBots::getPackagedBots(JokesterPackage::class)->toArray());
+    }
+
+    /** @test */
+    public function it_can_be_installed()
+    {
+        $thread = $this->createGroupThread($this->tippin);
+        $this->actingAs($this->tippin);
+
+        $this->postJson(route('api.messenger.threads.bots.packages.store', [
+            'thread' => $thread->id,
+        ]), [
+            'alias' => 'jokester_package',
+        ])
+            ->assertSuccessful()
+            ->assertJson([
+                'actions_count' => 8,
+            ]);
+    }
+}
